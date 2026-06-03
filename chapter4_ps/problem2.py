1c9aa8b9c9f9edba00cb8edb5bbaff310aa84197
urvashisingh27 <urvashi.singh27@gmail.com>
2026-06-03 12:22:34 +0530

    chapter 4: practice set

---
+++ chapter4_ps/problem2.py
@@ -0,0 +1,17 @@
+marks = []
+
+m1 = int(input("marks of first stu: "))
+marks.append(m1)
+
+m2 = int(input("marks of first stu: "))
+marks.append(m2)
+
+m3 = int(input("marks of first stu: "))
+marks.append(m3)
+
+m4 = int(input("marks of first stu: "))
+marks.append(m4)
+
+print(marks)
+marks.sort()
+print(marks)
